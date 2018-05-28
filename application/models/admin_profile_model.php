36e1fea354808c749f0ece6d7b9837e0580e4c1e
Abdurrahman <capteinn@gmail.com>
2018-05-29 01:57:31 +0700

    fix error admin_profile

--- application/models/admin_profile_model.php
+++ application/models/admin_profile_model.php
@@ -5,9 +5,10 @@ class admin_profile_model extends CI_Model {
 	public function getProfile($id_users){
 		$this->db->select("*");
 		$this->db->from("users");
-		$this->db->join("roles","users.id_roles=roles.id_roles");
-		$this->db->where("roles.nama_roles","pengelola");
+		$this->db->join("roles", "roles.id_roles=users.id_roles");
 		$this->db->where("id_users",$id_users);
+		$this->db->where("users.id_roles",1);
+		$this->db->or_where("users.id_roles",4);
 		return $this->db->get()->result();
 	}
 
